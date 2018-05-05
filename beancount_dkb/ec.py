8e146bef754fb9296e6ee1201f6ff563fbe7e882
Gordon Schulz <gordon.schulz@gmail.com>
2018-05-05 10:26:11 +0200

    Black'd source file

--- beancount_dkb/ec.py
+++ beancount_dkb/ec.py
@@ -1,4 +1,5 @@
 import csv
+import re
 from datetime import datetime
 import locale
 
@@ -11,31 +12,37 @@ from ._common import change_locale, InvalidFormatError
 
 
 FIELDS = (
-    'Buchungstag',
-    'Wertstellung',
-    'Buchungstext',
-    'Auftraggeber / Begünstigter',
-    'Verwendungszweck',
-    'Kontonummer',
-    'BLZ',
-    'Betrag (EUR)',
-    'Gläubiger-ID',
-    'Mandatsreferenz',
-    'Kundenreferenz',
+    "Buchungstag",
+    "Wertstellung",
+    "Buchungstext",
+    "Auftraggeber / Begünstigter",
+    "Verwendungszweck",
+    "Kontonummer",
+    "BLZ",
+    "Betrag (EUR)",
+    "Gläubiger-ID",
+    "Mandatsreferenz",
+    "Kundenreferenz",
 )
 
 
 class ECImporter(importer.ImporterProtocol):
-    def __init__(self, iban, account, currency='EUR',
-                 numeric_locale='de_DE.UTF-8', file_encoding='utf-8'):
+
+    def __init__(
+        self,
+        iban,
+        account,
+        currency="EUR",
+        numeric_locale="de_DE.UTF-8",
+        file_encoding="utf-8",
+    ):
         self.iban = iban
         self.account = account
         self.currency = currency
         self.numeric_locale = numeric_locale
         self.file_encoding = file_encoding
 
-        self._expected_header = \
-            '"Kontonummer:";"{} / Girokonto";'.format(self.iban)
+        self._expected_header = '"Kontonummer:";"{} / Girokonto";'.format(self.iban)
         self._date_from = None
         self._date_to = None
         self._balance = None
@@ -67,20 +74,19 @@ class ECImporter(importer.ImporterProtocol):
         def _read_meta(fd):
             lines = [fd.readline().strip() for _ in range(3)]
 
-            reader = csv.reader(lines, delimiter=';',
-                                quoting=csv.QUOTE_MINIMAL, quotechar='"')
+            reader = csv.reader(
+                lines, delimiter=";", quoting=csv.QUOTE_MINIMAL, quotechar='"'
+            )
 
             for line in reader:
                 key, value, _ = line
 
-                if key.startswith('Von'):
-                    self._date_from = datetime.strptime(
-                        value, '%d.%m.%Y').date()
-                elif key.startswith('Bis'):
-                    self._date_to = datetime.strptime(
-                        value, '%d.%m.%Y').date()
-                elif key.startswith('Kontostand vom'):
-                    self._balance = locale.atof(value.rstrip(' EUR'), Decimal)
+                if key.startswith("Von"):
+                    self._date_from = datetime.strptime(value, "%d.%m.%Y").date()
+                elif key.startswith("Bis"):
+                    self._date_to = datetime.strptime(value, "%d.%m.%Y").date()
+                elif key.startswith("Kontostand vom"):
+                    self._balance = locale.atof(value.rstrip(" EUR"), Decimal)
 
         with change_locale(locale.LC_NUMERIC, self.numeric_locale):
             with open(file_.name, encoding=self.file_encoding) as fd:
@@ -97,41 +103,45 @@ class ECImporter(importer.ImporterProtocol):
                 _read_empty_line(fd)
 
                 # Data entries
-                reader = csv.DictReader(fd, delimiter=';',
-                                        quoting=csv.QUOTE_MINIMAL,
-                                        quotechar='"')
+                reader = csv.DictReader(
+                    fd, delimiter=";", quoting=csv.QUOTE_MINIMAL, quotechar='"'
+                )
 
                 for index, line in enumerate(reader):
                     meta = data.new_metadata(file_.name, index)
 
-                    amount = Amount(locale.atof(line['Betrag (EUR)'], Decimal),
-                                    self.currency)
-                    date = datetime.strptime(
-                        line['Buchungstag'], '%d.%m.%Y').date()
+                    amount = Amount(
+                        locale.atof(line["Betrag (EUR)"], Decimal), self.currency
+                    )
+                    date = datetime.strptime(line["Buchungstag"], "%d.%m.%Y").date()
 
-                    if line['Verwendungszweck'] == 'Tagessaldo':
+                    if line["Verwendungszweck"] == "Tagessaldo":
                         entries.append(
-                            data.Balance(meta, date, self.account, amount,
-                                         None, None)
+                            data.Balance(meta, date, self.account, amount, None, None)
                         )
                     else:
-                        payee = '{} {}'.format(
-                            line['Buchungstext'],
-                            line['Auftraggeber / Begünstigter']
+                        payee = "{} {}".format(
+                            line["Buchungstext"], line["Auftraggeber / Begünstigter"]
                         )
-                        narration = line['Verwendungszweck'].trim()
+                        narration = line["Verwendungszweck"].trim()
                         # often there's a lot of extra whitespace in here, remove them
                         narration = re.sub("\s{2,}", " ", narration)
 
                         postings = [
-                            data.Posting(self.account, amount, None, None,
-                                         None, None)
+                            data.Posting(self.account, amount, None, None, None, None)
                         ]
 
                         entries.append(
-                            data.Transaction(meta, date, self.FLAG, payee,
-                                             narration, data.EMPTY_SET,
-                                             data.EMPTY_SET, postings)
+                            data.Transaction(
+                                meta,
+                                date,
+                                self.FLAG,
+                                payee,
+                                narration,
+                                data.EMPTY_SET,
+                                data.EMPTY_SET,
+                                postings,
+                            )
                         )
 
             return entries
